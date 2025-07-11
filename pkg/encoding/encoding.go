fa9648938fbb9fe4dbf9ae07587dd00af978c75c
Dinesh Salunke <dineshsalunke@Dineshs-MacBook-Air.local>
2025-07-11 06:45:03 +0530

    refactor: encode state vector v1 implementation

--- pkg/encoding/encoding.go
+++ pkg/encoding/encoding.go
@@ -1,9 +1,47 @@
 package encoding
 
-import "github.com/dineshsalunke/ygo/pkg/core"
+import (
+	"slices"
+
+	"github.com/dineshsalunke/ygo/pkg/core"
+	"github.com/dineshsalunke/ygo/pkg/encoding/binary"
+)
+
+func writeStateVector(enc DsEncoder, sv map[uint64]uint64) error {
+	if err := enc.Encoder().WriteVarUint(uint64(len(sv))); err != nil {
+		return err
+	}
+	clients := make([]uint64, len(sv))
+	for client := range sv {
+		clients = append(clients, client)
+	}
+	slices.Sort(clients)
+	slices.Reverse(clients)
+
+	for _, client := range clients {
+		clock := sv[client]
+		if err := enc.Encoder().WriteVarUint(client); err != nil {
+			return err
+		}
+		if err := enc.Encoder().WriteVarUint(clock); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func writeDocumentStateVector(enc DsEncoder, doc *core.Doc) error {
+	return writeStateVector(enc, doc.Store().GetStateVector())
+}
 
 func EncodeStateVectorV1(doc *core.Doc) ([]byte, error) {
-	panic("not implemented")
+	encoder := &DsEncoderV1{
+		encoder: binary.NewEncoder([]byte{}),
+	}
+	if err := writeDocumentStateVector(encoder, doc); err != nil {
+		return nil, err
+	}
+	return encoder.Bytes(), nil
 }
 
 func EncodeStateVector(doc *core.Doc) ([]byte, error) {

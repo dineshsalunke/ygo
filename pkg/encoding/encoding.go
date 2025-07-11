90750de6ffc9d1739bf06e054b48b90858feab87
Dinesh Salunke <dineshsalunke@Dineshs-MacBook-Air.local>
2025-07-11 06:45:15 +0530

    refactor: rename from v2 to v1

--- pkg/encoding/encoding.go
+++ pkg/encoding/encoding.go
@@ -48,12 +48,12 @@ func EncodeStateVector(doc *core.Doc) ([]byte, error) {
 	return EncodeStateVectorV1(doc)
 }
 
-func EncodeStateAsUpdateV2(doc *core.Doc, encodedTargetStateVector []byte) ([]byte, error) {
+func EncodeStateAsUpdateV1(doc *core.Doc, encodedTargetStateVector []byte) ([]byte, error) {
 	panic("not implemented")
 }
 
 func EncodeStateAsUpdate(doc *core.Doc, encodedTargetStateVector []byte) ([]byte, error) {
-	return EncodeStateAsUpdateV2(doc, encodedTargetStateVector)
+	return EncodeStateAsUpdateV1(doc, encodedTargetStateVector)
 }
 
 func ApplyUpdateV1(doc *core.Doc, update []byte, txorigin any) error {

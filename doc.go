9f8d108a971cf95357916e6c26ba24217556acd7
Dinesh Salunke <dineshsalunke@Dineshs-MacBook-Air.local>
2025-07-12 19:50:24 +0530

    refactor: get map, array and text methods

--- doc.go
+++ doc.go
@@ -19,3 +19,59 @@ func NewDoc() *Doc {
 	return newDoc()
 }
 
+func (doc *Doc) GetMap(name string) (*YMap, error) {
+	var item *YMap
+	var check bool
+
+	st, check := doc.share[name]
+	if check {
+		item, check = st.(*YMap)
+		if !check {
+			return nil, fmt.Errorf("type with name %s is already defined with different type", name)
+		}
+	} else {
+		item = &YMap{}
+		item.Integrate(doc, nil)
+		doc.share[name] = item
+	}
+
+	return item, nil
+}
+
+func (doc *Doc) GetArray(name string) (*YArray, error) {
+	var item *YArray
+	var check bool
+
+	st, check := doc.share[name]
+	if check {
+		item, check = st.(*YArray)
+		if !check {
+			return nil, fmt.Errorf("type with name %s is already defined with different type", name)
+		}
+	} else {
+		item = &YArray{}
+		item.Integrate(doc, nil)
+		doc.share[name] = item
+	}
+
+	return item, nil
+}
+
+func (doc *Doc) GetText(name string) (*YText, error) {
+	var item *YText
+	var check bool
+
+	st, check := doc.share[name]
+	if check {
+		item, check = st.(*YText)
+		if !check {
+			return nil, fmt.Errorf("type with name %s is already defined with different type", name)
+		}
+	} else {
+		item = &YText{}
+		item.Integrate(doc, nil)
+		doc.share[name] = item
+	}
+
+	return item, nil
+}

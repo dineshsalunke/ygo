766e1db6c71e518be175d4ec4afc676e15e34868
Dinesh Salunke <dineshsalunke@Dineshs-MacBook-Air.local>
2025-07-12 19:47:42 +0530

    refactor: id struct

---
+++ id.go
@@ -0,0 +1,17 @@
+package ygo
+
+type ID struct {
+	client uint64
+	clock  uint64
+}
+
+func newID(client, clock uint64) *ID {
+	return &ID{
+		client: client,
+		clock:  clock,
+	}
+}
+
+func NewID(client, clock uint64) *ID {
+	return newID(client, clock)
+}

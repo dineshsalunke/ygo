4d7dfd9c648fe1b89eeba6eb4e8242c419b470ec
Dinesh Salunke <dineshsalunke@Dineshs-MacBook-Air.local>
2025-07-12 19:49:35 +0530

    refactor: get state vector and get state methods on struct store

--- struct_store.go
+++ struct_store.go
@@ -19,3 +19,24 @@ func newStructStore() *StructStore {
 	}
 }
 
+func (ss *StructStore) GetStateVector() ClientClockMap {
+	sv := make(ClientClockMap, len(ss.clients))
+	for client, structs := range ss.clients {
+		s := structs[len(structs)-1]
+		sv[client] = s.ID().clock + s.Length()
+	}
+	return sv
+}
+
+func (ss *StructStore) GetState(client uint64) uint64 {
+	structs, ok := ss.clients[client]
+	if !ok {
+		return 0
+	}
+	lastStruct := structs[len(structs)-1]
+	if !ok {
+		return 0
+	}
+
+	return lastStruct.ID().clock + lastStruct.Length()
+}

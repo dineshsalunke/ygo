395cd5549665c1ec12cae566983f5742a98ef3b9
Dinesh Salunke <dineshsalunke@Dineshs-MacBook-Air.local>
2025-07-13 09:26:01 +0530

    refactor: update encoder interface

---
+++ update_encoder.go
@@ -0,0 +1,24 @@
+package ygo
+
+type DsEncoder interface {
+	Bytes() []byte
+	ResetDsCurVal() error
+	WriteDsClock(clock uint64) error
+	WriteDsLength(l uint64) error
+}
+
+type UpdateEncoder interface {
+	DsEncoder
+	WriteLeftID() (*ID, error)
+	WriteRightID() (*ID, error)
+	WriteClient(client uint64) error
+	WriteInfo(info uint64) error
+	WriteString(str string) error
+	WriteParentInfo(info bool) error
+	WriteTypeRef(ref uint64) error
+	WriteLength(l uint64) error
+	WriteAny(a any) error
+	WriteBuf(buf []byte) error
+	WriteJSON(buf any) error
+	WriteKey(key string) error
+}

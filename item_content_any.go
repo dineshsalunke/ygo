d69e67dc5c83b3990c7f15b252a3e7a0a0564d2f
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-07-27 15:42:43 +0530

    refactor: any item content type initial implementation

---
+++ item_content_any.go
@@ -0,0 +1,37 @@
+package ygo
+
+type ItemContentAny struct {
+	contents []any
+}
+
+func newItemContentAny(contents []any) *ItemContentAny {
+	return &ItemContentAny{
+		contents: contents,
+	}
+}
+
+func (content *ItemContentAny) Length() uint64 {
+	return uint64(len(content.contents))
+}
+
+func (content *ItemContentAny) IsCountable() bool {
+	return true
+}
+
+func init() {
+	decoders[BlockKindItemAny] = func(decoder UpdateDecoder, info Kind) (ItemContent, error) {
+		length, err := decoder.ReadLength()
+		if err != nil {
+			return nil, err
+		}
+		values := make([]any, 0, length)
+		for range length {
+			val, err := decoder.ReadAny()
+			if err != nil {
+				return nil, err
+			}
+			values = append(values, val)
+		}
+		return newItemContentAny(values), nil
+	}
+}

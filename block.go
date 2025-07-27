3dfd3623f3a913b05a6e6c22182504f1904cbcfe
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-07-27 11:55:19 +0530

    refactor: add constructors for GC and Skip

--- block.go
+++ block.go
@@ -18,12 +18,24 @@ func (self *block) Length() uint64 {
 
 type GC struct{ *block }
 
+func newGC(id *ID, length uint64) *GC {
+	return &GC{
+		block: &block{id: id, length: length},
+	}
+}
+
 func (self *GC) Kind() Kind {
 	return BlockKindGC
 }
 
 type Skip struct {
 	*block
+}
+
+func newSkip(id *ID, length uint64) *GC {
+	return &GC{
+		block: &block{id: id, length: length},
+	}
 }
 
 func (self *Skip) Kind() Kind {

a989898ba5541db3961d0250be73c6be1e1842ba
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-07-27 15:41:37 +0530

    refactor: item_content decoder and registry

--- item_content.go
+++ item_content.go
@@ -1,7 +1,24 @@
 package ygo
 
-type ItemContent interface{}
+import "fmt"
 
-func decode_item_content() (ItemContent, error) {
-	panic("not implemented")
+type item_content_decoder func(decoder UpdateDecoder, info Kind) (ItemContent, error)
+
+var decoders map[Kind]item_content_decoder = make(map[Kind]item_content_decoder, 0)
+
+type ItemContent interface {
+	IsCountable() bool
+	Length() uint64
+}
+
+func decode_item_content(decoder UpdateDecoder, info Kind) (ItemContent, error) {
+	item_decoder, ok := decoders[Kind(info&31)]
+	if !ok {
+		return nil, fmt.Errorf("unregistered item content kind : %d", info)
+	}
+	content, err := item_decoder(decoder, info)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
 }

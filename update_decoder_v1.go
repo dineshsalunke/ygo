0688b9206fcc0e76329e43d9dc5d3c9766feaa14
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-07-27 09:57:30 +0530

    refactor: typecast uint64 to clientid

--- update_decoder_v1.go
+++ update_decoder_v1.go
@@ -27,7 +27,7 @@ func (dec *UpdateDecoderV1) ReadLeftID() (*ID, error) {
 	if err != nil {
 		return nil, err
 	}
-	return newID(client, clock), nil
+	return newID(ClientID(client), clock), nil
 }
 
 func (dec *UpdateDecoderV1) ReadRightID() (*ID, error) {
@@ -39,11 +39,12 @@ func (dec *UpdateDecoderV1) ReadRightID() (*ID, error) {
 	if err != nil {
 		return nil, err
 	}
-	return newID(client, clock), nil
+	return newID(ClientID(client), clock), nil
 }
 
 func (dec *UpdateDecoderV1) ReadClient() (ClientID, error) {
-	return dec.ReadVarUint()
+	client_id, err := dec.ReadVarUint()
+	return ClientID(client_id), err
 }
 
 func (dec *UpdateDecoderV1) ReadInfo() (byte, error) {

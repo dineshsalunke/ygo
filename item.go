8eb56eda9dd00bff004adfc9e8716247bb6348fb
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-07-27 15:40:20 +0530

    refactor: decode item implementation

--- item.go
+++ item.go
@@ -17,6 +17,56 @@ func (self *Item) Kind() Kind {
 	panic("not implemented")
 }
 
-func decode_item(id *ID, decoder UpdateDecoder, info byte) (*Item, error) {
-	panic("not implemented")
+func decode_item(id *ID, decoder UpdateDecoder, info Kind) (*Item, error) {
+	var err error
+	var item *Item = &Item{}
+	cant_copy_parent_info := byte(info)&(HasOriginFlag|HasRightOriginFlag) == 0
+
+	if byte(info)&HasOriginFlag != 0 {
+		item.origin, err = decoder.ReadLeftID()
+		if err != nil {
+			return nil, err
+		}
+	}
+	if byte(info)&HasRightOriginFlag != 0 {
+		item.right_origin, err = decoder.ReadRightID()
+		if err != nil {
+			return nil, err
+		}
+	}
+	if cant_copy_parent_info {
+		parent_info, err := decoder.ReadParentInfo()
+		if err != nil {
+			return nil, err
+		}
+		if parent_info {
+			// Parent string
+			_, err := decoder.ReadVarString()
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			item.parent, err = decoder.ReadLeftID()
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		if (byte(info) & HasParentSubFlag) != 0 {
+			item.parent_sub, err = decoder.ReadVarString()
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	item.content, err = decode_item_content(decoder, info)
+	if err != nil {
+		return nil, err
+	}
+	item.block = &block{
+		id:     id,
+		length: item.content.Length(),
+	}
+	return item, nil
 }

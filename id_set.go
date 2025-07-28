02ec71eb6eee6f4e4116915523c9ad7fa70ee3e8
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-07-28 09:16:49 +0530

    refactor: constructor method for the id set

--- id_set.go
+++ id_set.go
@@ -4,6 +4,9 @@ type IdSet struct {
 	clients map[ClientID]*IdRanges
 }
 
+func newIdSet() *IdSet {
+	return &IdSet{}
+}
 
 func decode_id_set(decoder UpdateDecoder) (IdSet, error) {
 	panic("not implemented")

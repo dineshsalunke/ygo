e8b14ac5af09700ae5431f1dc4167354d31629e6
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-07-28 10:56:42 +0530

    refactor: add helper method for finding range in ranges using binary search
    instead of doing a custom function we could probably use
    BinarySearchFunc method from the slices package

--- id_set.go
+++ id_set.go
@@ -20,3 +20,26 @@ func (set *IdSet) addRanges(client ClientID, clock, length uint64) {
 		}
 	}
 }
+// TODO: we could probably use the BinarySearchFunc from the slices package
+// Find Range start in the ranges slice using binary search
+func findRangeStartInIdRanges(ids []*IdRange, clock uint64) (int, bool) {
+	left := 0
+	right := len(ids) - 1
+	for left <= right {
+		midindex := left + right/2
+		mid := ids[midindex]
+		midclock := mid.clock
+		if midclock <= clock {
+			if clock < mid.end() {
+				return midindex, true
+			}
+			left = midindex + 1
+		} else {
+			right = midindex - 1
+		}
+	}
+	if left < len(ids) {
+		return left, true
+	}
+	return 0, false
+}

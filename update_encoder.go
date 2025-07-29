522ac9e591dddb60f4d52682db505b317eb6cdb9
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-07-29 21:40:56 +0530

    refactor: update encoder interface

---
+++ update_encoder.go
@@ -0,0 +1,29 @@
+package ygo
+
+type UpdateEncoder interface {
+	ResetDsCurrVal()
+	WriteDsClock(length uint64) error
+	WriteDsLength(length uint64) error
+	WriteLeftID(id *ID) error
+	WriteRightID(id *ID) error
+	WriteClient(clientId ClientID) error
+	WriteInfo(info byte) error
+	WriteParentInfo(hasParentInfo bool) error
+	WriteTypeRef(ref byte) error
+	WriteLength(length uint64) error
+	WriteAny(val any) error
+	WriteJson(val any) error
+	WriteKey(key string) error
+
+	//
+	WriteUint8(val byte) error
+	WriteUint16(val uint16) error
+	WriteUint32(val uint32) error
+	WriteVarUint(val uint64) error
+	WriteVarint(val int64) error
+	WriteUint8Array(val []byte) error
+	WriteVarUint8Array(val []byte) error
+	WriteVarString(val string) error
+	WriteFloat32(val float32) error
+	WriteFloat64(val float64) error
+}

6ca9f7c5203a9996c2b0ff67afa1e7b116b5d911
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-08-01 19:58:58 +0530

    refactor: remove the early aliasing of clientid

--- id.go
+++ id.go
@@ -2,25 +2,23 @@ package ygo
 
 import "fmt"
 
-type ClientID uint64
-
 type ID struct {
-	client ClientID
+	client uint64
 	clock  uint64
 }
 
-func newID(client ClientID, clock uint64) *ID {
+func newID(client uint64, clock uint64) *ID {
 	return &ID{
 		client: client,
 		clock:  clock,
 	}
 }
 
-func NewID(client ClientID, clock uint64) *ID {
+func NewID(client uint64, clock uint64) *ID {
 	return newID(client, clock)
 }
 
 // Implement the fmt Stringer interface
 func (id *ID) String() string {
-	return fmt.Sprintf("%d:%d", id.client, id.clock)
+	return fmt.Sprintf("ID{client:%d,clock:%d}", id.client, id.clock)
 }

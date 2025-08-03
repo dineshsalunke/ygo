d1abb74d1582d3729e5a52f293bc1417343431eb
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-08-03 22:20:32 +0530

    refactor: implement Stringer interface for idset

--- id_set.go
+++ id_set.go
@@ -1,5 +1,9 @@
 package ygo
 
+import (
+	"fmt"
+	"strings"
+)
 type IdSet struct {
 	clients map[uint64]*IdRanges
 }
@@ -8,5 +12,15 @@ func newIdSet() *IdSet {
 	return &IdSet{
 		clients: make(map[uint64]*IdRanges),
 	}
+}
+
+func (ss *IdSet) GoString() string {
+	builder := strings.Builder{}
+	builder.WriteString("IdSet {\r\tclients: {")
+	for client, ranges := range ss.clients {
+		builder.WriteString(fmt.Sprintf("%d:%#v\n", client, ranges))
+	}
+	builder.WriteString("}\r}")
+	return builder.String()
 }
 

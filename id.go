13345dea19673507d62acfb4c1d472fc95b1f772
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-08-08 08:56:46 +0530

    refactor: helper method to check if ids are equal

--- id.go
+++ id.go
@@ -22,3 +22,13 @@ func NewID(client uint64, clock uint64) *ID {
 func (id *ID) GoString() string {
 	return fmt.Sprintf("ID{client:%d,clock:%d}", id.client, id.clock)
 }
+
+func (id *ID) Equals(other *ID) bool {
+	if id == other {
+		return true
+	}
+	if id != nil && other != nil {
+		return id.clock == other.clock && id.client == other.client
+	}
+	return false
+}

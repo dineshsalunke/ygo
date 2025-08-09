bf2ce9d588389398a46b49676cc8d51bc8462de8
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-08-09 09:01:03 +0530

    refactor: item content interface partial implementation for item content any

--- item_content_any.go
+++ item_content_any.go
@@ -4,6 +4,34 @@ type ItemContentAny struct {
 	contents []any
 }
 
+func (self *ItemContentAny) Content() any {
+	panic("not implemented") // TODO: Implement
+}
+
+func (self *ItemContentAny) Copy() ItemContent {
+	panic("not implemented") // TODO: Implement
+}
+
+func (self *ItemContentAny) MergeWith(right ItemContent) bool {
+	if cany, ok := right.(*ItemContentAny); ok {
+		self.contents = append(self.contents, cany.contents...)
+		return true
+	}
+	return false
+}
+
+func (self *ItemContentAny) Integrate(tx *Transaction, item Block) error {
+	return nil
+}
+
+func (self *ItemContentAny) Delete(tx *Transaction) error {
+	return nil
+}
+
+func (self *ItemContentAny) GC(tx *Transaction) error {
+	return nil
+}
+
 func (content *ItemContentAny) Write(encoder UpdateEncoder, offset uint64, offsetEnd uint64) error {
 	end := uint64(len(content.contents)) - offset
 	if err := encoder.WriteLength(end - offset); err != nil {
@@ -19,30 +47,30 @@ func (content *ItemContentAny) Write(encoder UpdateEncoder, offset uint64, offse
 	return nil
 }
 
+func (self *ItemContentAny) Length() uint64 {
+	return uint64(len(self.contents))
+}
+
+func (self *ItemContentAny) IsCountable() bool {
+	return true
+}
+
+func (self *ItemContentAny) GetRef() Kind {
+	return BlockKindItemAny
+}
+
+func (self *ItemContentAny) Splice(offset uint64) ItemContent {
+	c := make([]any, uint64(len(self.contents)-int(offset)))
+	copy(c, self.contents[offset:])
+	self.contents = self.contents[:offset]
+	right := newItemContentAny(c)
+	return right
+}
+
 func newItemContentAny(contents []any) *ItemContentAny {
 	return &ItemContentAny{
 		contents: contents,
 	}
-}
-
-func (content *ItemContentAny) Length() uint64 {
-	return uint64(len(content.contents))
-}
-
-func (content *ItemContentAny) IsCountable() bool {
-	return true
-}
-
-func (content *ItemContentAny) GetRef() Kind {
-	return BlockKindItemAny
-}
-
-func (content *ItemContentAny) Splice(offset uint64) ItemContent {
-	c := make([]any, uint64(len(content.contents)-int(offset)))
-	copy(c, content.contents[offset:])
-	content.contents = content.contents[:offset]
-	right := newItemContentAny(c)
-	return right
 }
 
 func init() {

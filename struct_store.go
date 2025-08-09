9037ab7e6c6e419fa9512651528e51ed27e1b202
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-08-09 09:00:22 +0530

    refactor: integrate structs and write blocks methods

--- struct_store.go
+++ struct_store.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"slices"
 )
+
 type PendingStructs struct {
 	missingState StateVector
 	update       []byte
@@ -26,8 +27,62 @@ func newStructStore() *StructStore {
 	}
 }
 
-func (ss *StructStore) IntegrateStructs(tx *Transaction) (*PendingStructs, error) {
-	panic("not implemented")
+func writeBlocks(encoder UpdateEncoder, blocks []Block, client uint64, idRanges []*IdRange) error {
+	blocksToWrite := 0
+
+	type tempStruct struct {
+		start      uint64
+		end        uint64
+		startClock uint64
+		endClock   uint64
+	}
+	indexRanges := make([]tempStruct, 0)
+	firstPossibleClock := blocks[0].ClockStart()
+	lastBlock := blocks[len(blocks)-1]
+	lastPossibleClock := lastBlock.ClockEnd()
+
+	for _, idRange := range idRanges {
+		startClock := max(idRange.clock, firstPossibleClock)
+		endClock := min(idRange.clock+idRange.length, lastPossibleClock)
+		if startClock < endClock {
+			startIndex, _ := binarySearchClockIndex(blocks, startClock)
+			endIndex, _ := binarySearchClockIndex(blocks, endClock-1)
+			blocksToWrite += endIndex - startIndex
+			indexRanges = append(indexRanges, tempStruct{start: uint64(startIndex), end: uint64(endIndex) + 1, startClock: startClock, endClock: endClock})
+		}
+	}
+
+	blocksToWrite += len(idRanges) - 1
+	clock := indexRanges[0].startClock
+	if err := encoder.WriteVarUint(uint64(blocksToWrite)); err != nil {
+		return err
+	}
+	if err := encoder.WriteClient(client); err != nil {
+		return err
+	}
+	if err := encoder.WriteVarUint(clock); err != nil {
+		return err
+	}
+
+	for _, indexRange := range indexRanges {
+		skipLength := indexRange.startClock - clock
+		if skipLength > 0 {
+			newSkip(newID(client, clock), skipLength).Write(encoder, 0, 0)
+			clock += skipLength
+		}
+		for i := indexRange.start; i < indexRange.end; i++ {
+			block := blocks[i]
+			blockEnd := block.ClockEnd()
+			offsetEnd := max(blockEnd-indexRange.endClock, 0)
+			if err := block.Write(encoder, clock-block.ClockStart(), byte(offsetEnd)); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
 func writeClientsBlocks(encoder UpdateEncoder, ss *StructStore, sv StateVector) error {
 	sm := make(StateVector, 0)
 	for client, clock := range sv {
@@ -84,6 +139,157 @@ func (ss *StructStore) GetStateVector() StateVector {
 	return sm
 }
 
+func (ss *StructStore) IntegrateStructs(tx *Transaction, remoteBlockSet *StructSet) (*PendingStructs, error) {
+	stack := make([]Block, 0)
+	clientIds := make([]uint64, len(remoteBlockSet.clients))
+	i := 0
+	for client := range remoteBlockSet.clients {
+		clientIds[i] = client
+		i++
+	}
+	slices.SortFunc(clientIds, func(a, b uint64) int {
+		return int(b - a)
+	})
+
+	// Helper to get the next available struct batch to process.
+	// It iterates backwards through the sorted client IDs.
+	getNextBlockTarget := func() *StructRange {
+		for len(clientIds) > 0 {
+			clientID := clientIds[len(clientIds)-1]
+			target := remoteBlockSet.clients[clientID]
+			if target.i < len(target.refs) {
+				return target
+			}
+			// This client is done, pop it.
+			clientIds = clientIds[:len(clientIds)-1]
+		}
+		return nil
+	}
+
+	curBlockTarget := getNextBlockTarget()
+	if curBlockTarget == nil {
+		return nil, nil // Nothing to integrate
+	}
+
+	restBlocks := newStructStore()
+	missingSv := make(map[uint64]uint64, 0)
+
+	updateMissingSv := func(client, clock uint64) {
+		missingClock, exists := missingSv[client]
+		if !exists || missingClock > clock {
+			missingSv[client] = clock
+		}
+	}
+
+	addStackToSS := func() {
+		for _, item := range stack {
+			client := item.Client()
+			inapplicableItems, has := remoteBlockSet.clients[client]
+			if has {
+				inapplicableItems.i -= 1
+				restBlocks.clients[client] = inapplicableItems.refs[inapplicableItems.i:]
+				delete(remoteBlockSet.clients, client)
+				inapplicableItems.i = 0
+				inapplicableItems.refs = make([]Block, 0)
+			} else {
+				restBlocks.clients[client] = []Block{item}
+			}
+
+			filtered := make([]uint64, 0)
+			for _, id := range clientIds {
+				if id != client {
+					filtered = append(filtered, client)
+				}
+			}
+			clientIds = filtered
+		}
+		stack = make([]Block, 0)
+	}
+
+	curBlockTarget.i += 1
+	stackHead := curBlockTarget.refs[curBlockTarget.i]
+
+	state := make(map[uint64]uint64, 0)
+
+	for {
+		if _, ok := stackHead.(*Skip); !ok {
+			localClock, has := state[stackHead.Client()]
+			if !has {
+				localClock = ss.GetClientClockEnd(stackHead.Client())
+				state[stackHead.Client()] = localClock
+			}
+
+			offset := int(localClock - stackHead.ClockStart())
+			missing, hasMissing, err := stackHead.GetMissing(tx, ss)
+			if err != nil {
+				return nil, err
+			}
+			if hasMissing {
+				stack = append(stack, stackHead)
+
+				blockRefs, hasBlockRefs := remoteBlockSet.clients[missing]
+				missingFromStack := slices.ContainsFunc(stack, func(b Block) bool {
+					return b.Client() == missing
+				})
+				if !hasBlockRefs ||
+					blockRefs.i == len(blockRefs.refs) ||
+					missing == stackHead.Client() ||
+					missingFromStack {
+					updateMissingSv(missing, ss.GetClientClockEnd(missing))
+					addStackToSS()
+				} else if hasBlockRefs {
+					blockRefs.i += blockRefs.i
+					stackHead = blockRefs.refs[blockRefs.i]
+					continue
+				}
+				//
+			} else {
+				if offset < 0 {
+					skip := newSkip(newID(stackHead.Client(), localClock), uint64(-offset))
+					if err := skip.Integrate(tx, 0); err != nil {
+						return nil, err
+					}
+				}
+				if err := stackHead.Integrate(tx, 0); err != nil {
+					return nil, err
+				}
+				state[stackHead.Client()] = max(stackHead.ClockEnd(), localClock)
+			}
+		}
+
+		if len(stack) > 0 {
+			stackHead = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+		} else if curBlockTarget != nil && curBlockTarget.i < len(curBlockTarget.refs) {
+			stackHead = curBlockTarget.refs[curBlockTarget.i]
+			curBlockTarget.i += 1
+		} else {
+			curBlockTarget = getNextBlockTarget()
+			if curBlockTarget == nil {
+				break
+			} else {
+				stackHead = curBlockTarget.refs[curBlockTarget.i]
+				curBlockTarget.i += 1
+			}
+		}
+	}
+
+	if len(restBlocks.clients) > 0 {
+		encoder := newUpdateEncoderV1()
+		if err := writeClientsBlocks(encoder, restBlocks, make(StateVector)); err != nil {
+			return nil, err
+		}
+		if err := encoder.WriteVarUint(0); err != nil {
+			return nil, err
+		}
+
+		return &PendingStructs{
+			update:       encoder.Bytes(),
+			missingState: missingSv,
+		}, nil
+	}
+	return nil, nil
+}
 
 func (ss *StructStore) ApplyIdSet(decoder UpdateDecoder, tx *Transaction) ([]byte, error) {
 	unappliedIdSet := newIdSet()

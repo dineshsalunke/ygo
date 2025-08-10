e8d106ff03fe254d16505874b02ec640448f2811
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-08-10 07:32:16 +0530

    refactor: GetMap method for doc

--- doc.go
+++ doc.go
@@ -33,6 +33,33 @@ func (doc *Doc) get(key string) (SharedType, error) {
 	return t, nil
 }
 
+func (doc *Doc) GetMap(key string) (*YMap, error) {
+	share, has := doc.share[key]
+	if has {
+		ymap, isymap := share.(*YMap)
+		if isymap {
+			return ymap, nil
+		}
+
+		basetype, isbasetype := share.(*BaseSharedType)
+		if isbasetype {
+			ymap := &YMap{
+				BaseSharedType: basetype,
+			}
+			ymap.Integrate(doc, nil)
+			doc.share[key] = ymap
+			return ymap, nil
+		}
+	}
+
+	ymap := newYMap(make(map[string]any))
+	if err := ymap.Integrate(doc, nil); err != nil {
+		return nil, err
+	}
+
+	return ymap, nil
+}
+
 func (doc *Doc) GetItemKey(item any) (string, bool) {
 	for key, t := range doc.share {
 		if reflect.TypeOf(t) == reflect.TypeOf(item) {

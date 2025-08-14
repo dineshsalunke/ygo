2b0a2fbe9779118aec486e6642421a4bb4a78051
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-08-14 22:13:35 +0530

    refactor: add helper first and last block method

--- block_range.go
+++ block_range.go
@@ -4,3 +4,11 @@ type BlockRange struct {
 	i    int
 	refs BlockList
 }
+
+func (self *BlockRange) FirstBlock() Block {
+	return self.refs[0]
+}
+
+func (self *BlockRange) LastBlock() Block {
+	return self.refs[0]
+}

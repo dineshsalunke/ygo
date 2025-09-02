6363e27b306e7c22d32c4bbd40a36a0b9fa4ee38
Dinesh Salunke <dineshsalunke@consultbop.com>
2025-09-02 08:44:27 +0530

    refactor: set method on ymap

--- ymap.go
+++ ymap.go
@@ -1,6 +1,9 @@
 package ygo
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 type YMap struct {
 	*BaseSharedType
@@ -39,8 +42,42 @@ func (ymap *YMap) Delete(key string, tx *Transaction) error {
 	return nil
 }
 
-func (ymap *YMap) Set(key string, value any) {
-	panic("not implemented")
+func (ymap *YMap) Set(key string, value any) (any, error) {
+	if ymap.doc != nil {
+		return Transact(ymap.doc, func(tx *Transaction) (any, error) {
+			left, _ := ymap.blocks[key]
+			doc := tx.doc
+			ownClientId := doc.clientID
+			var content ItemContent = nil
+			if value == nil {
+				content = newItemContentAny([]any{value})
+			} else {
+				switch value.(type) {
+				case int, *int, int8, *int8, int16, *int16, int32, *int32, int64, *int64, uint8, *uint8, uint16, *uint16, uint32, *uint32, uint64, *uint64, string, *string, bool, *bool:
+					content = newItemContentAny([]any{value})
+				case []byte, []*byte:
+					panic("not implemented")
+				case *Doc, Doc:
+					panic("not implemented")
+				default:
+					if st, ok := value.(SharedType); ok {
+						content = newItemContentType(st)
+					} else {
+						return nil, fmt.Errorf("failed to integrate item for key %s, unexpected value type %T", key, value)
+					}
+				}
+			}
+			nextClock := doc.store.GetClientClockEnd(ownClientId)
+			item := newItem(newID(ownClientId, nextClock), WithLeftLastID(left), WithContent(content), WithParent(ymap), WithParentSub(key))
+			if err := item.Integrate(tx, 0); err != nil {
+				return value, fmt.Errorf("failed to integrate item for key %s value %#v: %w", key, value, err)
+			}
+			return value, nil
+		}, nil, true)
+	} else {
+		ymap.prelimContent[key] = value
+	}
+	return value, nil
 }
 
 func (ymap *YMap) Get(key string) (any, bool) {
